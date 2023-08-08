cc6a8c91510aee295e987acc4417ec21422a2eef
LordsResistanceArmy <LordsResistanceArmy@outlook.com>
2023-08-09 07:13:44 +0800

    moves

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -12,6 +12,7 @@ fn main() {
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
+    let mut vec1 = vec1;
     vec1.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
