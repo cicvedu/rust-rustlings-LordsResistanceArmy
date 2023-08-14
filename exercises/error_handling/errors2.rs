e22c6e8caaec820f31b769fabfd85ec45cfb372c
LordsResistanceArmy <LordsResistanceArmy@outlook.com>
2023-08-15 06:31:12 +0800

    errorhandling

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -28,7 +28,11 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Ok(i) => Ok(i * cost_per_item + processing_fee),
+        Err(_) => qty
+    }
+    
 }
 
 #[cfg(test)]
