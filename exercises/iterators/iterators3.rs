fb8a8c47261ea028131a898f34059f11871497ce
LordsResistanceArmy <LordsResistanceArmy@outlook.com>
2023-08-19 07:35:30 +0800

    iterators

--- exercises/iterators/iterators3.rs
+++ exercises/iterators/iterators3.rs
@@ -26,15 +26,29 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    }
+    if a%b == 0 {
+        Ok(a/b)
+    } else {
+        Err(DivisionError::NotDivisible(NotDivisibleError { dividend: a, divisor: b }))
+    }
 }
 
 // Complete the function and return a value of the correct type so the test
 // passes.
 // Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+fn result_with_list() -> Result<i32, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    for i in division_results{
+        match i {
+            Ok(r) => numbers.append(r),
+            Err(e) => return Err(e)
+        };
+        Ok(numbers)
+    }
 }
 
 // Complete the function and return a value of the correct type so the test
