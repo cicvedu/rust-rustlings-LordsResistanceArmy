0ad19b568d8611eea5e8f6976a5f57e35e6e39ae
LordsResistanceArmy <LordsResistanceArmy@outlook.com>
2023-08-20 23:42:22 +0800

    drive_end

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,7 +1,5 @@
-use std::env;
 fn main() {
     let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
-    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-env=TEST_FOO={}", timestamp);
     println!("cargo:rustc-cfg=feature=\"pass\"");
 }
--- exercises/drive/drive2.rs
+++ exercises/drive/drive2.rs
@@ -3,27 +3,19 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-
 struct Foo {
     a: u128,
     b: Option<String>,
-}
-
-impl Foo {
-    fn modify_b(&mut self) {
-        self.b = Some("hello".to_owned());
-    }
 }
 
 fn raw_pointer_to_box(address: usize) -> Box<Foo> {
     // address is a pointer that points to heap.
     // construct Box from this address, and modify Foo's b field to 
     // the string "hello"
-    unsafe {
-        Box::from_raw(address as *mut Foo)
-    }
+
+    let mut buf = unsafe { Box::from_raw(address as *mut Foo)};
+    buf.b = Some("hello".to_string());
+    buf
 }
 
 
@@ -36,19 +28,17 @@ mod tests {
     fn test_success() {
         let nanos = Instant::now().duration_since(Instant::now().checked_sub(std::time::Duration::new(0, 1)).unwrap()).as_nanos();
 
-        let mut data = Box::new(Foo{
+        let data = Box::new(Foo{
             a: nanos,
             b: None
         });
 
         let ptr_1 = &data.a as *const u128 as usize;
-        let mut ret = raw_pointer_to_box(Box::into_raw(data) as usize);
+        let ret = raw_pointer_to_box(Box::into_raw(data) as usize);
 
         let ptr_2 = &ret.a as *const u128 as usize;
 
         assert!(ptr_1 == ptr_2);
-
-        ret.modify_b();
         assert!(ret.b == Some("hello".to_owned()));
 
     }
